c8b6adad1e03839075c34d1b13c8958196f951ec
hirendhola <hirendholap@example.com>
2024-02-14 21:46:03 +0530

    changed cors permissions

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,7 @@ require("dotenv").config()
 
 const app = express()
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://authentication-autorization-frontend.vercel.app',
     methods: ['POST', 'GET', 'DELETE', 'PUT'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
